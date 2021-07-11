8f15472737a739195c16ca21a225e4d0875994ef
learning333 <simia.teste@gmail.com>
2021-07-10 22:11:31 -0300

    passo 0

---
+++ src/main/java/com/example/teste0/Teste0Application.java
@@ -0,0 +1,13 @@
+package com.example.teste0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Teste0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Teste0Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/teste0/Teste0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teste0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Teste0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
